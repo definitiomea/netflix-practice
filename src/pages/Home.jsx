8647320067ffb385e7481ad8d283fafb9962692e
Johnny_P <shinezl39@gmail.com>
2022-11-17 11:51:30 +0900

    Effect Hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 const Home = () => {
+    useEffect(() => {
+        /* API 호출, 비동기로
+        리덕스 미들웨어, 혹은 툴킷으로 처리하기 */
+    }, []);
+
     return (
         <>
         
