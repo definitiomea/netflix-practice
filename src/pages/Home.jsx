7805b89f2d5cf781e72d8e3b14b2b55ab5853f9a
Johnny_P <shinezl39@gmail.com>
2022-11-18 12:30:08 +0900

    ~ slide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Banner from "../Components/Banner";
 import { movieAction } from "../redux/actions/movieAction";
-import { useDispatch } from "react-redux";
 
 const Home = () => {
     const dispatch = useDispatch();
+    const { popularMovies, topRatedMovies, upComingMovies } = useSelector((state) => state.movie);
+    /* 호출을 확인한 코드
+    console.log(popularMovies, topRatedMovies, upComingMovies); */
 
     useEffect(() => {
         dispatch(movieAction.getMovies());
@@ -11,7 +15,11 @@ const Home = () => {
 
     return (
         <>
-        
+            {popularMovies.results && <Banner movie={popularMovies.results[0]}></Banner>}
+            <h1>Popular Movie</h1>
+            {/* 호출할 컴포넌트들... */}
+            <h1>Top Rated Movie</h1>
+            <h1>Upcoming Movie</h1>
         </>
     );
 }
